9f06c303be3058621e4ba2a3b7b8c64082e4f53b
Nicolas Burrus <nicolas@burrus.name>
2020-06-14 12:01:50 +0200

    ImPlot: add support for thread_local.

--- imgui/imconfig.h
+++ imgui/imconfig.h
@@ -13,6 +13,8 @@
 
 #pragma once
 
+#define IMPLOT_THREAD_LOCAL thread_local
+
 //---- Define assertion handler. Defaults to calling assert().
 // If your macro uses multiple statements, make sure is enclosed in a 'do { .. } while (0)' block so it can be used as a single statement.
 //#define IM_ASSERT(_EXPR)  MyAssert(_EXPR)
